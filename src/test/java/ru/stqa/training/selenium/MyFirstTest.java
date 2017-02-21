3717c9a7198ff8e1c54233cc58a11c5b89e1b1f1
Victoria Kadubina <kotenok@devpro.fritz.box>
2017-02-22 00:01:52 +0100

    my first test

---
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -0,0 +1,40 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+/**
+ * Created by kotenok on 21/02/17.
+ */
+public class MyFirstTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Google-Suche"));
+    }
+
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
