bde298ed54a54c0ae88300111b03365c050fd076
Victoria Kadubina <kotenok@devpro.fritz.box>
2017-02-23 21:51:10 +0100

    added negative case

--- src/test/java/ru/stqa/training/selenium/LoginInAdminTest.java
+++ src/test/java/ru/stqa/training/selenium/LoginInAdminTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.*;
 
 /**
  * Created by vkadubina on 2/23/2017.
@@ -24,11 +24,21 @@ public class LoginInAdminTest {
 
     @Test
     public void loginInAdminTest() {
-        driver.get("http://localhost/litecart/admin/");
+        driver.get(adminUrl);
         driver.findElement(By.name("password")).sendKeys("not-so-secret-password");
         driver.findElement(By.name("login")).click();
         assertFalse(driver.getCurrentUrl().contains("login.php"));
     }
+
+    @Test
+    public void wrongPasswordTest() {
+        driver.get(adminUrl);
+        driver.findElement(By.name("password")).sendKeys("wrong-password");
+        driver.findElement(By.name("login")).click();
+        assertTrue(driver.getCurrentUrl().contains("login.php"));
+    }
+
+    private String adminUrl = "http://localhost/litecart/admin/";
 
 
     @After
---
+++ src/test/java/ru/stqa/training/selenium/MySecondTest.java
@@ -0,0 +1,49 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+
+
+/**
+ * Created by kotenok on 22/02/17.
+ */
+public class MySecondTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://localhost:8080/");
+        List<WebElement> elements = driver.findElements(By.cssSelector("li.product"));
+        assertTrue(elements.size() > 0);
+
+        elements.get(0).click();
+        wait.until(urlContains("rubber-ducks-c-1/subcategory-c-2"));
+    }
+
+
+
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
