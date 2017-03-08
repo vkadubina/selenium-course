759917eecaee208d4316214bf323fbb4817c4078
OFFICE\vkadubina <vkadubina@devexperts.com>
2017-03-08 17:09:58 +0100

    Modified OpenProductPageTest;

--- src/test/java/ru/stqa/training/selenium/OpenCorrectProductPageTest.java
+++ src/test/java/ru/stqa/training/selenium/OpenCorrectProductPageTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.openqa.selenium.support.ui.ExpectedConditions.urlContains;
@@ -22,29 +24,55 @@ public class OpenCorrectProductPageTest extends MultiBrowserBaseTest{
     }
 
     @Test
-    public void clickOnProductInCampaginsOpenCorrectPage(){
+    public void clickOnProductInCampaignsOpenCorrectPage(){
         driver.get(url);
         assertCampaginsContainsProducts();
         WebElement productOnMainPage = driver.findElement((By.cssSelector("div#box-campaigns ul.products li:first-child")));
-        ArrayList<String> mainPageProductParameters = getProductParameters(productOnMainPage);
+        ArrayList<String> mainPageProductParameters = getProductParameters(productOnMainPage, true);
 
 
         productOnMainPage.findElement(By.cssSelector("a")).click();
         wait.until(urlContains("rubber-ducks"));
+
         WebElement productOnSelfPage = driver.findElement((By.cssSelector("div#box-product")));
-        ArrayList<String> selfPageProductParameters = getProductParameters(productOnSelfPage);
+        ArrayList<String> selfPageProductParameters = getProductParameters(productOnSelfPage,false);
 
-
+        assertTrue(mainPageProductParameters.equals(selfPageProductParameters));
 
     }
 
-    private ArrayList<String> getProductParameters(WebElement productOnMainPage) {
+    private ArrayList<String> getProductParameters(WebElement product, boolean isMainPage) {
         ArrayList<String> parameters = new ArrayList<>();
-        parameters.add(productOnMainPage.findElement(By.cssSelector("div.name")).getText());
-        parameters.add(productOnMainPage.findElement(By.cssSelector("div.price-wrapper s.regular-price")).getText().replace("$",""));
-        parameters.add(productOnMainPage.findElement(By.cssSelector("div.price-wrapper strong.campaign-price")).getText().replace("$",""));
+        String name;
 
+        if (isMainPage) {
+            name = product.findElement(By.cssSelector("div.name")).getText();}
+        else {
+            name = product.findElement(By.cssSelector("[itemprop=name]")).getText();
+        }
 
+        WebElement regularPrice = product.findElement(By.cssSelector("div.price-wrapper s.regular-price"));
+        WebElement campaignPrice = product.findElement(By.cssSelector("div.price-wrapper strong.campaign-price"));
+
+        assertTrue(regularPrice.getCssValue("text-decoration").equals("line-through"));
+        assertTrue(campaignPrice.getCssValue("font-weight").equals("bold"));
+
+        System.out.println("regularPriceColor = " + regularPrice.getCssValue("color"));
+        System.out.println("campaignPriceColor = " + campaignPrice.getCssValue("color"));
+
+        assertRegularPriceSizeLessThanCampaign(regularPrice,campaignPrice);
+
+        parameters.add(name);
+        parameters.add(regularPrice.getText().replace("$",""));
+        parameters.add(campaignPrice.getText().replace("$",""));
+
+        return parameters;
+    }
+
+    private void assertRegularPriceSizeLessThanCampaign(WebElement price1, WebElement price2) {
+        double size1 = Double.parseDouble(price1.getCssValue("font-size").replaceAll("[a-zA-Z]", ""));
+        double size2 = Double.parseDouble(price2.getCssValue("font-size").replaceAll("[a-zA-Z]", ""));
+        assertTrue(size1 < size2);
     }
 
     private void assertCampaginsContainsProducts(){
@@ -52,5 +80,6 @@ public class OpenCorrectProductPageTest extends MultiBrowserBaseTest{
         assertTrue(campaginsProductsCount > 0);
     }
 
-    String url="http://localhost:8080/";
+    //String url="http://localhost:8080/";
+    String url="http://192.168.99.100:8080/";
 }
