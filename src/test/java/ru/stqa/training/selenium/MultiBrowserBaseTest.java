a3e57ef5f4ab09d7e066821e5f09dd43199a2562
vkadubina <vkadubina@gmail.com>
2017-03-12 18:46:56 +0100

    Added WorkWithCartTest; Updated selenium to 3.3.1.

--- src/test/java/ru/stqa/training/selenium/MultiBrowserBaseTest.java
+++ src/test/java/ru/stqa/training/selenium/MultiBrowserBaseTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -29,8 +30,11 @@ abstract public class MultiBrowserBaseTest {
 
     @Parameterized.Parameters
     public static Collection<Object> data() {
-        return Stream.of("chrome", "firefox", "firefox45","firefox-nightly", "safari")
+        return Stream.of("chrome","firefox","firefox-nightly"
+                //"safari","firefox45"
+        )
                 .collect(Collectors.toList());
+
     }
 
     private String browser;
@@ -93,17 +97,21 @@ abstract public class MultiBrowserBaseTest {
     private void initFF45esrDriver() {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability(FirefoxDriver.MARIONETTE, false);
-        FirefoxBinary bin = new FirefoxBinary(new File("/Applications/Firefox45.app/Contents/MacOS/firefox"));
-        driver = new FirefoxDriver(bin, new FirefoxProfile(),caps);
+        FirefoxOptions ffo = new FirefoxOptions();
+        ffo.setBinary(new FirefoxBinary(new File("/Applications/Firefox45.app/Contents/MacOS/firefox")));
+        ffo.setProfile(new FirefoxProfile());
+        ffo.addDesiredCapabilities(caps);
+        driver = new FirefoxDriver(ffo);
         wait = new WebDriverWait(driver, 10);
 
     }
 
     private void initFFNightlyDriver() {
-        FirefoxBinary bin = new FirefoxBinary(new File("/Applications/FirefoxNightly.app/Contents/MacOS/firefox"));
-        driver = new FirefoxDriver(bin, new FirefoxProfile());
+        FirefoxOptions ffo = new FirefoxOptions();
+        ffo.setProfile(new FirefoxProfile());
+        ffo.setBinary(new FirefoxBinary(new File("/Applications/FirefoxNightly.app/Contents/MacOS/firefox")));
+        driver = new FirefoxDriver(ffo);
         wait = new WebDriverWait(driver, 10);
-
     }
 
 
@@ -119,5 +127,14 @@ abstract public class MultiBrowserBaseTest {
         wait = new WebDriverWait(driver, 10);
     }
 
-
+    public void loginInAdmin(WebDriver driver, String url){
+        if (driver != null) {
+            driver.get(url);
+            driver.findElement(By.name("username")).clear();
+            driver.findElement(By.name("username")).sendKeys("admin");
+            driver.findElement(By.name("password")).sendKeys("not-so-secret-password");
+            driver.findElement(By.name("login")).click();
+            wait.until(urlToBe(url));
+        }
+    }
 }
